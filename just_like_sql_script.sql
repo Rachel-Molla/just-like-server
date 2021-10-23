7b971ad67fc6ab63a832a7a7d75973e5fca9ae81
Rachel <mollarachel2@gmail.com>
2021-10-23 21:17:58 +0300

    update user accounts table in db

--- just_like_sql_script.sql
+++ just_like_sql_script.sql
@@ -10,14 +10,19 @@ use just_like_db;
 show tables;
 
 CREATE TABLE IF NOT EXISTS `just_like_accounts` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `type` enum( "admin", "manager" , "volunteer" , "junior" ),
+  `id` int NOT NULL AUTO_INCREMENT unique,
+  `type` enum( "task_manager" , "volunteer" , "junior" ),
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
-  `number_phone` INT NOT NULL,
+  `number_phone` int NOT NULL,
+  `linkedin_profile` varchar(100) NOT NULL,
+  `google_account` varchar(100),
+  `twitter_account` varchar(100),
+  `facebook_account` varchar(100),
+  `areas_of_interest` SET( "job_search", "lecture", "workshop" ),
   CONSTRAINT PK_Account PRIMARY KEY (ID)
 );
 
