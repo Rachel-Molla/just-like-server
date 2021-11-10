c2810b3038a00f9f0b793349e7a7d97273751d47
Rachel <mollarachel2@gmail.com>
2021-11-10 12:41:05 +0200

    remove duplicate files

--- helpers/jwt-helper.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken"); 
-
-function getNewToken(payload) { // (payload will be the user object)
-    return jwt.sign({ payload }, config.jwtKey, { expiresIn: "30m" });
-}
-
-module.exports = {
-    getNewToken
-};
