7dc953b836821fe935071796b91aca77f68570e8
Rachel <mollarachel2@gmail.com>
2021-11-10 15:17:56 +0200

    adding comment to mysql connection

--- express-server/data-access-layer/dal.js
+++ express-server/data-access-layer/dal.js
@@ -18,6 +18,7 @@ function executeAsync(sql,values) {
     return new Promise((resolve, reject) => {
         connection.query(sql, values,(err, result) => {
             if(err) return reject(err);
+            console.log("MySQL connect successfully");
             resolve(result);
         });
     });
